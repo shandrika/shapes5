4d2d78f097c782c49d038907a4200d9d5d1c643a
csPython <cspython@AD25TF04NGF1J-64.local>
2019-02-15 13:21:04 -0500

    Second Shape Created: Rectangle

--- shapes5SG.py
+++ shapes5SG.py
@@ -10,3 +10,9 @@ window.setCoords(0,0,1000,1000)
 triangle = Polygon (Point(0,0),Point(50,100),Point(100,0))
 triangle.setFill(color_rgb(30,30,230))
 triangle.draw (window)
+
+#Second Shape: Red Square
+
+square = Rectangle(Point(1000,0),Point(900,100))
+square.setFill(color_rgb(230,30,30))
+square.draw (window)
