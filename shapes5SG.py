ec99d3af9cddfa9e3608988c36a59a62ca9e52ae
csPython <cspython@AD25TF04NGF1J-64.local>
2019-02-15 13:32:48 -0500

    Third Shape: Circle

--- shapes5SG.py
+++ shapes5SG.py
@@ -7,7 +7,7 @@ window.setCoords(0,0,1000,1000)
 
 #First Shape: Blue Triangle
 
-triangle = Polygon (Point(0,0),Point(50,100),Point(100,0))
+triangle = Polygon(Point(0,0),Point(50,100),Point(100,0))
 triangle.setFill(color_rgb(30,30,230))
 triangle.draw (window)
 
@@ -16,3 +16,8 @@ triangle.draw (window)
 square = Rectangle(Point(1000,0),Point(900,100))
 square.setFill(color_rgb(230,30,30))
 square.draw (window)
+
+#Third Shape: Pink Circle
+circle = Circle(Point(60,940),60)
+circle.setFill(color_rgb(255,123,146))
+circle.draw (window)
