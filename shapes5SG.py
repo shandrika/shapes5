09dedf57cd2277feaf69f075b5a1b86ea244ae6a
csPython <cspython@AD25TF04NGF1J-64.local>
2019-02-15 09:46:48 -0500

    First Shape Created

--- shapes5SG.py
+++ shapes5SG.py
@@ -1,4 +1,12 @@
 from graphics import*
 
+#Create Window
+
 window = GraphWin("5 Shapes",1000,1000)
 window.setCoords(0,0,1000,1000)
+
+#First Shape: Blue Triangle
+
+triangle = Polygon (Point(0,0),Point(50,100),Point(100,0))
+triangle.setFill(color_rgb(30,30,230))
+triangle.draw (window)
